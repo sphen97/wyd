a6374e24593f5fd51ac1638195f211c606916556
CamMortus <47607472+CamMortus@users.noreply.github.com>
2019-11-23 02:22:05 -0500

    Update creatingSuperUser.sh

--- wyd_project/creatingSuperUser.sh
+++ wyd_project/creatingSuperUser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "creating super user"
-python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'cmortus@knights.ucf.edu', 'adminpass')"
+python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'Your_Email_Here@test.com', 'adminpass')"
